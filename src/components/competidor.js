f68c9e45996d7ba8b6e28faa0f1657f6f59a7137
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-04 17:09:07 -0300

    component created

---
+++ src/components/competidor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function competidor() {
+    return(
+        <div className='contenedor-competidor'>
+            <img className='imagen-competidor'
+            src={'../imagenes/imagen-gazir.jfif'}
+            alt='imagen gazir'/>
+        </div>
+    )
+}
