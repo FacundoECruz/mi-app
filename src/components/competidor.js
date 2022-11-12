725b58c4601716f24d8092abf8bb306bdfe3f4f9
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-12 16:08:07 -0300

    component props

--- src/components/competidor.js
+++ src/components/competidor.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../stylesheets/Competidor.css'
 
-function Competidor() {
+function Competidor(props) {
     return(
         <div className='contenedor-competidor'>
             <img className='imagen-competidor'
             src={require('../images/imagen-gazir.jpg')}
             alt='imagen gazir'/>
             <div className='contenedor-texto-competidor'>
-                <p className='nombre-competidor'>Texto</p>
-                <p className='titulo-competidor'>Texto</p>
-                <p className='frase-competidor'>Texto</p>
+                <p className='nombre-competidor'>{props.nombre} de {props.pais}</p>
+                <p className='titulo-competidor'>{props.titulo}</p>
+                <p className='frase-competidor'>{props.frase}</p>
             </div>
         </div>
     )
